3417f2edf70250246158e246757c06a1a86465af
dengwenbin <dengwenbin@banggood.com>
2018-05-28 11:44:09 +0800

    table中函数未定义的错误

--- static/interfacetest/iframe/table.js
+++ static/interfacetest/iframe/table.js
@@ -21,36 +21,36 @@ $(document).ready(function() {
             $("div.table tbody tr input[type='checkbox']:checked").trigger("click");
         }
     })
-    function getdata(table){    //获取table数据
-        var data={};
-        $("table." + table + " tbody").children("tr").each(function () {
-            var key=$(this).find(".key").val();
-            var value=$(this).find(".value").val();
-            if (key !=''){
-                data[key]=value;
-            }
-        })
-        return data;
-    }
-    function update(table,dit){     //更新table数据
-        $("table." + table + " tbody").html('');
-        var id=1;
-        for (i in dit){
-            content ="<tr trid="+id+">" +
-                "<td><input type=\"text\" class='key' tclass="+table+ " trid="+id+" value="+i+"></td>" +
-                "<td><input type=\"text\" class='value' tclass="+table+ " trid="+id+" value="+dit[i]+"></td>" +
-                "<td><input type=\"button\" value=\"X\" class='delbtn' tclass="+table+ " trid="+id+"></td>" +
-                "</tr>"
-            $("table." + table + " tbody").append(content);
-            id++
+
+})
+function getdata(table){    //获取table数据
+    var data={};
+    $("table." + table + " tbody").children("tr").each(function () {
+        var key=$(this).find(".key").val();
+        var value=$(this).find(".value").val();
+        if (key !=''){
+            data[key]=value;
         }
+    })
+    return data;
+}
+function update(table,dit){     //更新table数据
+    $("table." + table + " tbody").html('');
+    var id=1;
+    for (i in dit){
         content ="<tr trid="+id+">" +
-            "<td><input type=\"text\" class='key' tclass="+table+ " trid="+id+"></td>" +
-            "<td><input type=\"text\" class='value' tclass="+table+ " trid="+id+"></td>" +
+            "<td><input type=\"text\" class='key' tclass="+table+ " trid="+id+" value="+i+"></td>" +
+            "<td><input type=\"text\" class='value' tclass="+table+ " trid="+id+" value="+dit[i]+"></td>" +
             "<td><input type=\"button\" value=\"X\" class='delbtn' tclass="+table+ " trid="+id+"></td>" +
             "</tr>"
         $("table." + table + " tbody").append(content);
-
+        id++
     }
-})
+    content ="<tr trid="+id+">" +
+        "<td><input type=\"text\" class='key' tclass="+table+ " trid="+id+"></td>" +
+        "<td><input type=\"text\" class='value' tclass="+table+ " trid="+id+"></td>" +
+        "<td><input type=\"button\" value=\"X\" class='delbtn' tclass="+table+ " trid="+id+"></td>" +
+        "</tr>"
+    $("table." + table + " tbody").append(content);
 
+}
